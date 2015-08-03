8c96bf86d76791ca8a69ea04d7e3110fbd77a93c
hotshot <mrahmani1373@gmail.com>
2015-08-03 14:35:20 +0430

    Template for managing

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
+    url(r'^', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,15 +5,15 @@ from . import views
 urlpatterns = [
     # ex: /polls/
     url(r'^register/$', views.register, name='register'),
-     url(r'^eventsubmit/$', views.eventSubmit, name='eventSubmit'),
+    url(r'^eventsubmit/$', views.eventSubmit, name='eventSubmit'),
     url(r'^buy/$', views.buy, name='buy'),
     url(r'^about/$', views.about, name='about'),
     url(r'^login/$', views.login, name='login'),
     url(r'^main/$', views.main, name='main'),
-   url(r'^home/$', views.home, name='home'),
-     url(r'^event/$', views.event, name='event'),
-      url(r'^report/$', views.report, name='report'),
-      url(r'^contact/$', views.contact, name='contact'),
+    url(r'^home/$', views.home, name='home'),
+    url(r'^event/$', views.event, name='event'),
+    url(r'^report/$', views.report, name='report'),
+    url(r'^contact/$', views.contact, name='contact'),
     url(r'^eventreport/$', views.event_report, name='contact'),
     url(r'^manage/$', views.manage, name='manage')
 
